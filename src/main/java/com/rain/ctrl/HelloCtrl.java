b9466fdb1ac647cc534422e564027a58be11ace8
zhangjintao <gmzhangjintao@hotmail.com>
2025-04-23 12:00:21 +0800

    init

---
+++ src/main/java/com/rain/ctrl/HelloCtrl.java
@@ -0,0 +1,16 @@
+package com.rain.ctrl;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhang jintao on 2025/04/23
+ **/
+@RestController
+public class HelloCtrl {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
